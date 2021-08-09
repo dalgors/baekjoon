6f161cde776393b2b741357d1e7c0b5b0294b73b
solo5star <solo_5@naver.com>
2021-08-10 01:18:39 +0900

    install pylint and fix

--- baekjoon.py
+++ baekjoon.py
@@ -1,6 +1,8 @@
+import re
+import time
+import requests
 from bs4 import BeautifulSoup as bs
-import re, time, requests
-from requests.sessions import Request
+
 
 class CookieExpired(Exception):
     pass
@@ -18,14 +20,14 @@ class BaekjoonSession:
     __session: requests.Session
     __requestCount: int
 
-    """
-    백준 전용 세션을 생성합니다.
+    def __init__(self, groupId: int, cookies=None, requestLimit=5, throttlePerRequestAsMilliseconds=100):
+        """
+        백준 전용 세션을 생성합니다.
 
-    :param groupId: 백준 그룹의 아이디입니다
-    :param requestLimit: 프로그램에서 최대로 사용할 수 있는 요청 횟수입니다. 초과할 시 프로그램이 종료됩니다.
-    :param throttlePerRequestAsMilliseconds: 각 요청당 딜레이를 설정합니다. millisecond 단위로 설정합니다.
-    """
-    def __init__(self, groupId: int, cookies = {}, requestLimit = 5, throttlePerRequestAsMilliseconds = 100):
+        :param groupId: 백준 그룹의 아이디입니다
+        :param requestLimit: 프로그램에서 최대로 사용할 수 있는 요청 횟수입니다. 초과할 시 프로그램이 종료됩니다.
+        :param throttlePerRequestAsMilliseconds: 각 요청당 딜레이를 설정합니다. millisecond 단위로 설정합니다.
+        """
         self.groupId = groupId
         self.requestLimit = requestLimit
         self.throttlePerRequestAsMilliseconds = throttlePerRequestAsMilliseconds
@@ -35,14 +37,14 @@ class BaekjoonSession:
         # 실제 요청 횟수가 기록되는 변수
         self.__requestCount = 0
 
-    """
-    주어진 URL에 GET 리퀘스트를 보내어 응답을 받아옵니다.
-    내부적으로 requests.Session 객체를 사용하며, 요청 횟수 및 쓰로틀링을 위해
-    wrapping한 함수입니다.
-
-    :param url: 불러올 URL
-    """
     def get(self, url):
+        """
+        주어진 URL에 GET 리퀘스트를 보내어 응답을 받아옵니다.
+        내부적으로 requests.Session 객체를 사용하며, 요청 횟수 및 쓰로틀링을 위해
+        wrapping한 함수입니다.
+
+        :param url: 불러올 URL
+        """
         # 요청 전에, request limit를 넘지 않는지 검사.
         if self.__requestCount >= self.requestLimit:
             print(f'[Collector] 요청 횟수를 초과했습니다. 최대 요청 횟수={self.requestLimit}')
@@ -50,18 +52,20 @@ class BaekjoonSession:
 
         # 첫 번째 요청이 아니면 쓰로틀링 걸기
         if self.__requestCount != 0 and self.throttlePerRequestAsMilliseconds > 0:
-            print(f'[Collector] Throttle {self.throttlePerRequestAsMilliseconds} ms ...')
+            print(
+                f'[Collector] Throttle {self.throttlePerRequestAsMilliseconds} ms ...')
             time.sleep(self.throttlePerRequestAsMilliseconds / 1000)
 
         result = self.__session.get(url)
         self.__requestCount += 1
 
-        print(f'[Collector] "GET {url}" {result.status_code} request_remain={self.requestLimit - self.__requestCount}')
+        print(
+            f'[Collector] "GET {url}" {result.status_code} request_remain={self.requestLimit - self.__requestCount}')
 
         return result
 
     def ensureLogin(self):
-        if "가입 신청" in self.get(f"https://www.acmicpc.net/group/{self.groupId}").text:
+        if '가입 신청' in self.get(f'https://www.acmicpc.net/group/{self.groupId}').text:
             print('[Collector] 로그인이 되어있지 않습니다. 쿠키 정보를 확인해주세요.')
             raise CookieExpired
 
@@ -72,35 +76,37 @@ class BaekjoonSession:
 
         # 제출 번호, 유저 이름, 문제 번호, 문제 이름
         submission = {
-            'id': int(re.search("solution-(\d+)", tagStr)[1]),
-            'username': re.search("/user/(\w+)", tagStr)[1],
-            'problemId': int(re.search("/problem/(\d+)", tagStr)[1]),
-            'problemName': re.search('title="(.+?)"', tagStr)[1], # '?' at '.+?' means non-greedy (https://stackoverflow.com/questions/766372/python-non-greedy-regexes)
+            'id': int(re.search(r'solution-(\d+)', tagStr)[1]),
+            'username': re.search(r'/user/(\w+)', tagStr)[1],
+            'problemId': int(re.search(r'/problem/(\d+)', tagStr)[1]),
+            # '?' at '.+?' means non-greedy (https://stackoverflow.com/questions/766372/python-non-greedy-regexes)
+            'problemName': re.search(r'title="(.+?)"', tagStr)[1],
         }
 
         # 티어 정보를 사용할 수 있다면 티어 정보도 추가
         # 1=Bronze V, 2=Bronze IV, 3=Bronze III, 4=Bronze II, 5=Bronze I
         # 6=Silver V, 7=Silver IV ...
-        tier = re.search('/tier/(\d+)\.svg', tagStr)
+        tier = re.search(r'/tier/(\d+)\.svg', tagStr)
         if tier is not None:
             submission['tier'] = int(tier[1])
 
         # 결과 정보 파싱 및 저장
-        resultTag =  submissionTag.contents[3].contents[0].contents[0]
+        resultTag = submissionTag.contents[3].contents[0].contents[0]
         if 'class' not in resultTag.attrs:
             resultTag = resultTag.contents[0]
 
         submission.update({
-            'resultCode': '-'.join(resultTag.attrs['class'][0].split("-")[1:]).upper(),
-            'resultMessage': " ".join(resultTag.contents[0].split(u'\xa0'))
+            'resultCode': '-'.join(resultTag.attrs['class'][0].split('-')[1:]).upper(),
+            'resultMessage': ' '.join(resultTag.contents[0].split(u'\xa0'))
         })
 
         # 메모리, 시간, 언어, 코드 길이 정보
-        memory, time, language, length = map(lambda el: el.text, submissionTag.contents[4:8])
+        memory, timeRecord, language, length = [
+            el.text for el in submissionTag.contents[4:8]]
 
         submission.update({
             'memory': int(memory) if memory else None,
-            'time': int(time) if time else None,
+            'time': int(timeRecord) if timeRecord else None,
             'language': language,
             'length': int(length),
             'when': submissionTag.contents[8].contents[0].attrs['title']
@@ -108,20 +114,20 @@ class BaekjoonSession:
 
         return submission
 
-    def fetchSubmissions(self, top = None): 
-        url = f"https://www.acmicpc.net/status?group_id={self.groupId}"  
-        if top != None:
-            url += f"&top={top}"
-        
+    def fetchSubmissions(self, top=None):
+        url = f'https://www.acmicpc.net/status?group_id={self.groupId}'
+        if top is not None:
+            url += f'&top={top}'
+
         html = self.get(url)
         soup = bs(html.text, 'html.parser')
-        submissionsTags = soup.find("tbody").find_all("tr")
+        submissionsTags = soup.find('tbody').find_all('tr')
 
         # 채점 중, 채점 준비중 등 채점이 완료되지 않은 채점 발견 시 다음 번에 크롤링하도록 함
         # (이번 크롤링에서는 제외)
         # 가장 아래의 제출부터 체크하고, 채점이 완료되지 않은 제출 발견 시 바로 break
         submissions = []
-        for submission in reversed([*map(self.__parseSubmissionFromTableRowElement, submissionsTags)]):
+        for submission in reversed([self.__parseSubmissionFromTableRowElement(tag) for tag in submissionsTags]):
             if submission['resultCode'] in ['WAIT', 'REJUDGE-WAIT', 'COMPILE', 'JUDGING']:
                 break
 
@@ -142,9 +148,9 @@ class BaekjoonSession:
                 for submission in self.fetchSubmissions(top):
                     if submission['id'] <= submissionId:
                         return submissions
-                    
+
                     submissions.append(submission)
-                
+
                 # do not include 'top' submission
                 top = submissions[-1]['id'] - 1
                 page += 1
@@ -159,6 +165,6 @@ class BaekjoonSession:
 
         return {
             'name': soup.find(id='problem_title').text,
-            'tier': int(re.search('tier/(\d+)\.svg', soup.find(class_='solvedac-tier')['src'])[1]),
+            'tier': int(re.search(r'tier/(\d+)\.svg', soup.find(class_='solvedac-tier')['src'])[1]),
             'tags': [e.find('a').text for e in soup.find(id='problem_tags').find_all('li')],
         }
--- main.py
+++ main.py
@@ -1,44 +1,49 @@
-import os, json, sys
-from baekjoon import *
+import os
+import json
+import sys
+from baekjoon import BaekjoonSession, CookieExpired, RequestLimitExceed
 
 
-"""
-submissions.json 파일을 최신화합니다.
-
-:param session: 백준에 요청을 보낼 수 있도록 BaekjoonSession 객체를 주어야 합니다.
-"""
 def updateSubmissions(session: BaekjoonSession):
-    with open("submissions.json", mode="r+", encoding="UTF-8") as submissionsJson:
+    """
+    submissions.json 파일을 최신화합니다.
+
+    :param session: 백준에 요청을 보낼 수 있도록 BaekjoonSession 객체를 주어야 합니다.
+    """
+    with open('submissions.json', mode='r+', encoding='UTF-8') as submissionsJson:
         # 최근 제출들을 가져옵니다.
         submissions = json.load(submissionsJson)
 
-        top = submissions[0]["id"]
+        top = submissions[0]['id']
         recentSubmissions = session.fetchSubmissionsUntil(top)
 
         # submissions.json 파일의 상태가 최신이면 아무것도 하지 않음
         if len(recentSubmissions) == 0:
-            print("submissions.json 파일 상태가 최신입니다.")
-        
+            print('submissions.json 파일 상태가 최신입니다.')
+
         # 써야 할 데이터가 있으면 쓰기
         else:
             submissionsJson.seek(0)
-            submissionsJson.write(json.dumps(recentSubmissions + submissions, indent='\t', ensure_ascii=False))
+            submissionsJson.write(json.dumps(
+                recentSubmissions + submissions, indent='\t', ensure_ascii=False))
             submissionsJson.truncate()
-            print(f"성공적으로 크롤링을 완료하였습니다. {len(recentSubmissions)}건 추가됨")
+            print(f'성공적으로 크롤링을 완료하였습니다. {len(recentSubmissions)}건 추가됨')
 
 
-"""
-competitions.json 파일 내의 문제들 중 problems.json 에 없는 문제들을 업데이트합니다.
-
-:param session: 백준에 요청을 보낼 수 있도록 BaekjoonSession 객체를 주어야 합니다.
-"""
 def updateProblems(session: BaekjoonSession):
+    """
+    competitions.json 파일 내의 문제들 중 problems.json 에 없는 문제들을 업데이트합니다.
+
+    :param session: 백준에 요청을 보낼 수 있도록 BaekjoonSession 객체를 주어야 합니다.
+    """
     # competitions.json 파일을 읽어 problems.json에 어떤 문제가 없는지 확인합니다
-    competitions = json.load(open("competitions.json", mode="r", encoding="UTF-8"))
+    competitions = json.load(
+        open('competitions.json', mode='r', encoding='UTF-8'))
     problemsDiscovered = {}
 
-    with open("problems.json", mode="r+", encoding="UTF-8") as problemsJson:
-        problemsKnown = {int(problemId): problemData for problemId, problemData in json.load(problemsJson).items()}
+    with open('problems.json', mode='r+', encoding='UTF-8') as problemsJson:
+        problemsKnown = {int(problemId): problemData for problemId,
+                         problemData in json.load(problemsJson).items()}
 
         try:
             # 가장 최신의 competition 부터 처리하기 위해 reverse
@@ -59,7 +64,8 @@ def updateProblems(session: BaekjoonSession):
         if len(problemsDiscovered) > 0:
             problemsKnown.update(problemsDiscovered)
             problemsJson.seek(0)
-            problemsJson.write(json.dumps(dict(sorted(problemsKnown.items())), indent='\t', ensure_ascii=False))
+            problemsJson.write(json.dumps(
+                dict(sorted(problemsKnown.items())), indent='\t', ensure_ascii=False))
             problemsJson.truncate()
             print(f'문제 정보들을 {len(problemsDiscovered)}건 추가하였습니다.')
 
@@ -69,17 +75,17 @@ try:
     # GROUP_ID: 백준 그룹 ID
     # BOJ_AUTO_LOGIN: 로그인 관련 쿠키 값
     # ONLINE_JUDGE: 로그인 관련 쿠키 값
-    groupId = os.getenv("GROUP_ID")
-    cookies = { 
-        "bojautologin": os.getenv("BOJ_AUTO_LOGIN"),
-        "OnlineJudge": os.getenv("ONLINE_JUDGE")
+    groupId = os.getenv('GROUP_ID')
+    cookies = {
+        'bojautologin': os.getenv('BOJ_AUTO_LOGIN'),
+        'OnlineJudge': os.getenv('ONLINE_JUDGE')
     }
     session = BaekjoonSession(groupId, cookies)
 
     # 로그인 상태를 확인
     # 로그인이 안되어 있을 시 CookieExpired 에러를 발생시켜 프로그램 중단
     session.ensureLogin()
-    
+
     # submissions.json 최신화
     updateSubmissions(session)
 
@@ -87,4 +93,4 @@ try:
     updateProblems(session)
 
 except CookieExpired:
-    sys.exit(1) # 로그인 실패로 인한 종료는 정상 종료가 아님
+    sys.exit(1)  # 로그인 실패로 인한 종료는 정상 종료가 아님
